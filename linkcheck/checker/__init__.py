01160fbd9e98b595276bc5ef56895c2476d2fd36
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-03-18 19:34:57 -0400

    spelling: separator
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- linkcheck/checker/__init__.py
+++ linkcheck/checker/__init__.py
@@ -108,7 +108,7 @@ def get_url_from (base_url, recursion_level, aggregate,
     else:
         scheme = None
         if not (url or name):
-            # use filename as base url, with slash as path seperator
+            # use filename as base url, with slash as path separator
             name = base_url.replace("\\", "/")
     allowed_schemes = aggregate.config["allowedschemes"]
     # ignore local PHP files with execution directives
