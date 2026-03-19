e43aa04b5520b59fbb2adbe2b547aac940e633cd
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-03-19 09:32:41 -0400

    spelling: waste
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- tests/checker/test_news.py
+++ tests/checker/test_news.py
@@ -27,7 +27,7 @@ NNTP_SERVER = "news.uni-stuttgart.de"
 # info string returned by news server
 NNTP_INFO = u"200 news.uni-stuttgart.de InterNetNews NNRP server " \
             u"INN 2.7.1 ready (no posting)"
-# Most free NNTP servers are slow, so don't waist a lot of time running those.
+# Most free NNTP servers are slow, so don't waste a lot of time running those.
 NNTP_TIMEOUT_SECS = 30
 
 # disabled for now until some stable news server comes up
